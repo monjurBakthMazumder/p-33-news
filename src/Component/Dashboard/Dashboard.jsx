499fc6b4a7ea0c759a9b86c7cd678818cdd112ec
md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2023-09-20 00:07:35 -0700

    'update

--- src/Component/Dashboard/Dashboard.jsx
+++ src/Component/Dashboard/Dashboard.jsx
@@ -6,12 +6,14 @@ const Dashboard = () => {
     return (
         <div>
             <Header/>
-            <div className="flex">
-            <div className="drawer lg:drawer-open w-80">
+            <div className="lg:flex">
+            <div className="drawer lg:drawer-open lg:w-80 z-50">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
+                <div className="drawer-content flex">
                     {/* Page content here */}
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    <label  htmlFor="my-drawer-2" tabIndex={0} className="btn btn-ghost lg:hidden flex justify-end w-full pr-8 sm:pr-[10%] md:pr-[12%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
                 
                 </div> 
                 <div className="drawer-side">
